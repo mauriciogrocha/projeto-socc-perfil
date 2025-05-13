5694e406b163452aefc18aea15ca2dde962cec3e
Mateus Ribeiro <mateusribeirus@gmail.com>
2025-05-13 19:09:44 -0300

    Create PermissaoController

--- src/main/java/com/soccperfil/controllers/PermissaoController.java
+++ src/main/java/com/soccperfil/controllers/PermissaoController.java
@@ -1,4 +1,69 @@
 package com.soccperfil.controllers;
 
+import com.soccperfil.model.Permissao;
+import com.soccperfil.repository.PermissaoRepository;
+
+import com.soccperfil.model.Perfil;
+import com.soccperfil.repository.PerfilRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/permissoes")
 public class PermissaoController {
+
+    @Autowired
+    private PermissaoRepository permissaoRepository;
+
+    @Autowired
+    private PerfilRepository perfilRepository;
+
+    @GetMapping
+    public List<Permissao> listar() {
+        return permissaoRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<String> criar(@RequestBody Permissao permissao) {
+        permissaoRepository.save(permissao);
+        return ResponseEntity.ok("Permissao criada com sucesso!");
+    }
+
+    //URI final - permissoes/{permissaoId}/perfis/{perfilId}
+    @PutMapping("/{permissaoId}/perfis/{perfilId}")
+    public ResponseEntity<String> atribuir(@PathVariable Integer permissaoId, @PathVariable Integer perfilId) {
+        Optional<Permissao> permissaoOpt = permissaoRepository.findById(permissaoId);
+        Optional<Perfil> perfilOpt = perfilRepository.findById(perfilId);
+
+        if (permissaoOpt.isEmpty() || perfilOpt.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Perfil perfil = perfilOpt.get();
+        perfil.getPermissoes().add(permissaoOpt.get());
+        perfilRepository.save(perfil);
+
+        return ResponseEntity.ok("Permissão atribuida com sucesso!");
+    }
+
+    @DeleteMapping("/{permissaoId}/perfis/{perfilId}")
+    public ResponseEntity<String> deletar(@PathVariable Integer permissaoId, @PathVariable Integer perfilId) {
+        Optional<Permissao> permissaoOpt = permissaoRepository.findById(permissaoId);
+        Optional<Perfil> perfilOpt = perfilRepository.findById(perfilId);
+
+        if (permissaoOpt.isEmpty() || perfilOpt.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Perfil perfil = perfilOpt.get();
+        perfil.getPermissoes().remove(permissaoOpt.get());
+        perfilRepository.save(perfil);
+
+        return ResponseEntity.ok("Permissão removida com sucesso!");
+    }
 }
--- src/main/java/com/soccperfil/repository/PermissaoRepository.java
+++ src/main/java/com/soccperfil/repository/PermissaoRepository.java
@@ -1,4 +1,9 @@
 package com.soccperfil.repository;
 
-public interface PermissaoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.soccperfil.model.Permissao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissaoRepository extends JpaRepository<Permissao, Integer> {
 }
